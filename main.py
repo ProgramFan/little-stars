eff2994d0dd105a4d8742873136c60eba7d4f149
ProgramFan <zyangmath@gmail.com>
2023-02-01 23:32:07 +0800

    feat: create play_music function

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-count = 0
-index = 0
-
 def on_button_pressed_a():
     global count, index
     count = randint(0, 5)
@@ -27,7 +24,63 @@ def on_button_pressed_a():
     basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def play_music(notes: List[number], fraq: List[number], tempo: number):
+    global i, x, y
+    music.set_tempo(tempo)
+    i = 0
+    while i <= len(notes) - 1:
+        x = notes[i]
+        y = fraq[i]
+        if x == 0:
+            music.rest(beats[y])
+        else:
+            music.play_tone(freqs[x], beats[y])
+        i += 1
+
 def on_button_pressed_b():
-    music.play_tone(392, music.beat(BeatFraction.QUARTER))
-    music.play_tone(262, music.beat(BeatFraction.WHOLE))
+    global tunes_1, beats_1
+    tunes_1 = [1, 2, 3, 1, 1, 2, 3, 1, 3, 4, 5, 3, 4, 5, 3, 0]
+    beats_1 = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2]
+    play_music(tunes_1, beats_1, 120)
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+beats_1: List[number] = []
+tunes_1: List[number] = []
+y = 0
+x = 0
+i = 0
+index = 0
+count = 0
+beats: List[number] = []
+freqs: List[number] = []
+# music notes: 1-7 for central 1-7, 8-14 for high 1-7, -1~-7 for low 1-7
+freqs = [0,
+    262,
+    294,
+    330,
+    349,
+    392,
+    440,
+    494,
+    523,
+    587,
+    659,
+    698,
+    784,
+    880,
+    988,
+    247,
+    220,
+    196,
+    175,
+    165,
+    147,
+    131]
+# music beats: 0 for breve, 1 for double, 2 for whole, 3 for half, 4 for 1/4, 5 for 1/8, 6 for 1/16.
+beats = [music.beat(BeatFraction.BREVE),
+    music.beat(BeatFraction.DOUBLE),
+    music.beat(BeatFraction.WHOLE),
+    music.beat(BeatFraction.HALF),
+    music.beat(BeatFraction.QUARTER),
+    music.beat(BeatFraction.EIGHTH),
+    music.beat(BeatFraction.SIXTEENTH)]
--- main.ts
+++ main.ts
@@ -1,5 +1,3 @@
-let count = 0
-let index = 0
 input.onButtonPressed(Button.A, function () {
     count = randint(0, 5)
     basic.showString("MUSIC")
@@ -26,7 +24,100 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(1000)
     basic.clearScreen()
 })
+function play_music (notes: number[], fraq: number[], tempo: number) {
+    music.setTempo(tempo)
+    i = 0
+    while (i <= notes.length - 1) {
+        x = notes[i]
+        y = fraq[i]
+        if (x == 0) {
+            music.rest(beats[y])
+        } else {
+            music.playTone(freqs[x], beats[y])
+        }
+        i += 1
+    }
+}
 input.onButtonPressed(Button.B, function () {
-    music.playTone(392, music.beat(BeatFraction.Quarter))
-    music.playTone(262, music.beat(BeatFraction.Whole))
+    tunes_1 = [
+    1,
+    2,
+    3,
+    1,
+    1,
+    2,
+    3,
+    1,
+    3,
+    4,
+    5,
+    3,
+    4,
+    5,
+    3,
+    0
+    ]
+    beats_1 = [
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    1,
+    2,
+    2,
+    1,
+    2,
+    2
+    ]
+    play_music(tunes_1, beats_1, 120)
 })
+let beats_1: number[] = []
+let tunes_1: number[] = []
+let y = 0
+let x = 0
+let i = 0
+let index = 0
+let count = 0
+let beats: number[] = []
+let freqs: number[] = []
+// music notes: 1-7 for central 1-7, 8-14 for high 1-7, -1~-7 for low 1-7
+freqs = [
+0,
+262,
+294,
+330,
+349,
+392,
+440,
+494,
+523,
+587,
+659,
+698,
+784,
+880,
+988,
+247,
+220,
+196,
+175,
+165,
+147,
+131
+]
+// music beats: 0 for breve, 1 for double, 2 for whole, 3 for half, 4 for 1/4, 5 for 1/8, 6 for 1/16.
+beats = [
+music.beat(BeatFraction.Breve),
+music.beat(BeatFraction.Double),
+music.beat(BeatFraction.Whole),
+music.beat(BeatFraction.Half),
+music.beat(BeatFraction.Quarter),
+music.beat(BeatFraction.Eighth),
+music.beat(BeatFraction.Sixteenth)
+]
